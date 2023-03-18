42132f59d3db7c35ed97c7d6a4940b9bbdc4d3be
Igor <dsfdkvj453@gmail.com>
2023-03-18 13:30:04 +0200

    added class: TestAlert added tests : -test_see_alert -test_alert_appear_5_sec -test_confirm_alert -test_prompt_alert

--- pages/alerts_frames_windows_page.py
+++ pages/alerts_frames_windows_page.py
@@ -1,9 +1,12 @@
-from locators.alerts_frames_windows_locators import BrowserWindowsPageLocators
+from selenium.common import UnexpectedAlertPresentException
+
+from locators.alerts_frames_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.base_page import BasePage
+import random
+import time
 
 
 class BrowserWindowsPage(BasePage):
-
     locators = BrowserWindowsPageLocators()
 
     def check_open_new_tab(self):
@@ -17,3 +20,44 @@ class BrowserWindowsPage(BasePage):
         self.driver.switch_to.window(self.driver.window_handles[1])
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
+
+class AlertsPage(BasePage):
+    locators = AlertsPageLocators()
+
+    def check_see_alert(self):
+        self.element_is_visible(self.locators.SEE_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.element_is_visible(self.locators.APPEAR_ALERT_AFTER_5_SEC_BUTTON).click()
+        time.sleep(6)
+        try:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+        except UnexpectedAlertPresentException:
+            alert_window = self.driver.swich_to_alert
+            return alert_window.text
+
+    def check_confirm_alert(self):
+        self.element_is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.CONFIRM_RESULT).text
+        return text_result
+
+    def check_prompt_alert(self):
+        self.remove_footer()
+        text = f'autotest{random.randint(0,999)}'
+        self.element_is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.PROMPT_RESULT).text
+        return text,text_result
+
+
+
+
+
+
--- tests/alerts_frames_windows_test.py
+++ tests/alerts_frames_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frames_windows_page import BrowserWindowsPage
+from pages.alerts_frames_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertFrameWindow:
@@ -12,10 +12,34 @@ class TestAlertFrameWindow:
             text_result = browser_windows_page.check_open_new_tab()
             assert text_result == 'This is a sample page'
 
-
-
         def test_new_window(self, driver):
             browser_windows_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
             browser_windows_page.open()
             text_result = browser_windows_page.check_open_new_tab()
             assert text_result == 'This is a sample page'
+
+
+class TestAlert:
+    def test_see_alert(self, driver):
+        alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+        alert_page.open()
+        alert_text = alert_page.check_see_alert()
+        assert alert_text == 'You clicked a button'
+
+    def test_alert_appear_5_sec(self, driver):
+        alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+        alert_page.open()
+        alert_text = alert_page.check_alert_appear_5_sec()
+        assert alert_text == 'This alert appeared after 5 seconds'
+
+    def test_confirm_alert(self, driver):
+        alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+        alert_page.open()
+        alert_text = alert_page.check_confirm_alert()
+        assert alert_text == 'You selected Ok'
+
+    def test_prompt_alert(self, driver):
+        alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+        alert_page.open()
+        text, alert_text = alert_page.check_prompt_alert()
+        assert text in alert_text, 'Alert did not show'
