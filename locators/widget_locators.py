261d588121d8738650f2f4918c3cc76bbd9fd021
Igor <dsfdkvj453@gmail.com>
2023-03-23 11:10:16 +0200

    added class: TestWidgets added tests : -test_accordian

---
+++ locators/widget_locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class AccordianPageLocators:
+    SECTION_FIRST = (By.CSS_SELECTOR, 'div[id="section1Heading"]')
+    SECTION_CONTENT_FIRST = (By.CSS_SELECTOR, 'div[id="section1Content"]')
+    SECTION_SECOND = (By.CSS_SELECTOR, 'div[id="section2Heading"]')
+    SECTION_CONTENT_SECOND = (By.CSS_SELECTOR, 'div[id="section2Content"]')
+    SECTION_THIRD = (By.CSS_SELECTOR, 'div[id="section3Heading"]')
+    SECTION_CONTENT_THIRD = (By.CSS_SELECTOR, 'div[id="section3Content"]')
